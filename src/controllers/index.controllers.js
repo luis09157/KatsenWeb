04650a0fdf33abccd0b8b2e0566a2f8b2039c0e9
DESKTOP-RISF858\sthef <luisk2ify@gmail.com>
2023-05-03 17:36:14 -0600

    cambios ejs

--- src/controllers/index.controllers.js
+++ src/controllers/index.controllers.js
@@ -1,5 +1,6 @@
 import { pool } from "../db/db.js";
 
+
 export const ping = async (req, res) => {
     const [result] = await pool.query('select "Pong" as result');
     res.json(result[0])
--- src/routes/index.routes.js
+++ src/routes/index.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
 
 import {ping} from '../controllers/index.controllers.js'
-//import { index, ping } from "../controllers/index.rotes.js";
 
 const router = Router();
 
-//router.get("/", index);
+router.get("/",function (req, res) {
+    res.render('home');
+})
+
 
 router.get("/ping", ping);
 
